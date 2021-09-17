a94bdf9a16f642231ddcf1a872595f91c9b0bf43
Rio Ananta <rioananta1@gmail.com>
2021-09-17 08:34:55 +0700

    StatusBar Customization

--- app/src/main/java/com/ananta/ieco/SplashScreen.kt
+++ app/src/main/java/com/ananta/ieco/SplashScreen.kt
@@ -2,10 +2,17 @@ package com.ananta.ieco
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.res.Resources
+import android.content.res.Resources.Theme
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.view.View
+import android.view.WindowManager
+
+import androidx.appcompat.app.AppCompatActivity
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreen : AppCompatActivity() {
@@ -14,6 +21,11 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
         supportActionBar?.hide()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window.statusBarColor = resources.getColor(R.color.white_flat, null)
+        }
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this@SplashScreen, MainActivity::class.java)
