14f7633542b7ab70df109c560d583af17cc839c4
Rio Ananta <rioananta1@gmail.com>
2021-09-17 09:42:22 +0700

    Adding Share Button and Favorite Button

--- app/src/main/java/com/ananta/ieco/DetailPage.kt
+++ app/src/main/java/com/ananta/ieco/DetailPage.kt
@@ -2,8 +2,10 @@ package com.ananta.ieco
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class DetailPage : AppCompatActivity() {
 
@@ -21,10 +23,21 @@ class DetailPage : AppCompatActivity() {
         val tvName: TextView = findViewById(R.id.tv_item_name)
         val tvDetail: TextView = findViewById(R.id.tv_item_detail)
         val image: ImageView = findViewById(R.id.img_item_photo)
+        val btnFavorite: Button = findViewById(R.id.btn_favorite)
+        val btnShare: Button = findViewById(R.id.btn_share)
+
 
         val name = intent.getStringExtra(EXTRA_NAME)
         val detail = intent.getStringExtra(EXTRA_DETAIL)
         val photo = intent.getIntExtra(EXTRA_PHOTO, 0)
+
+        btnFavorite.setOnClickListener{
+            Toast.makeText(this, "Telah menambahkan $name kedalam Favorite!", Toast.LENGTH_SHORT).show()
+        }
+
+        btnShare.setOnClickListener{
+            Toast.makeText(this, "Telah membagikan $name", Toast.LENGTH_SHORT).show()
+        }
 
         tvName.text = name
         tvDetail.text = detail
